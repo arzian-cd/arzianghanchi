e0657d6b84a1cb55ebcfe3a4599958ada74811af
Arzianghanchi <arzian.ghanchi@clouddrove.com>
2026-01-19 21:31:19 +0000

    feat: pr title check workflow

--- examples/eks-nodegroup-with-existing-cluster/output.tf
+++
@@ -1,9 +0,0 @@
-output "node_group_role_arn" {
-  description = "ARN of the IAM role assigned to the EKS managed node group."
-  value       = module.node-group-role.arn
-}
-
-output "node_group_role_name" {
-  description = "Name of the IAM role assigned to the EKS managed node group."
-  value       = module.node-group-role.name
-}
